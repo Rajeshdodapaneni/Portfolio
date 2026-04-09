4250b8069b4517364ad6430408f13f6b91d678fd
Dodapaneni Rajesh <rajeshdodapaneni055@gmail.com>
2026-04-09 10:57:25 +0530

    changesdone

---
+++ contact.js
@@ -0,0 +1,11 @@
+function dwnld(URL, filename){
+    let link= document.createElement("a")
+
+    link.setAttribute("download",filename);
+    link.href= URL;
+
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
+
+}
